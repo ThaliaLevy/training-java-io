f7991986d450e166c9578b63bd259754b6ab5e3f
ThaliaLevy <thalialevy@hotmail.com>
2022-03-08 15:26:52 -0300

    att

--- src/exerciciosFixacao/ex05.java
+++ src/exerciciosFixacao/ex05.java
@@ -5,7 +5,7 @@ import java.util.Locale;
 public class ex05 {
 
 	public static void main(String[] args) {
-		// String teste = "3";
+		//String teste = "3";
 		int valor1 = 6;
 		String imprimir = String.format(Locale.CHINA, "%08d", valor1);
 
